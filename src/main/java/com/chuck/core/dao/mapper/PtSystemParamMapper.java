4b41da9e3bc6dc9d8cdabfa6db363ba1aa39acad
shun.zhang@ilinklink.com <shun.zhang@ilinklink.com>
2020-12-08 14:05:18 +0800

    fc

---
+++ src/main/java/com/chuck/core/dao/mapper/PtSystemParamMapper.java
@@ -0,0 +1,16 @@
+package com.chuck.core.dao.mapper;
+
+import com.chuck.core.dao.entity.PtSystemParam;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 系统参数 Mapper 接口
+ * </p>
+ *
+ * @author chuck
+ * @since 2020-12-08
+ */
+public interface PtSystemParamMapper extends BaseMapper<PtSystemParam> {
+
+}
